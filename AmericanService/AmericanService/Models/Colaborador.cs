71756ced02dae1e22aa7a1fcbc98c3965d788f44
andressancho <36581472+andressancho@users.noreply.github.com>
2018-09-12 02:01:26 -0600

    Clases

--- AmericanService/AmericanService/Models/Colaborador.cs
+++ AmericanService/AmericanService/Models/Colaborador.cs
@@ -5,7 +5,19 @@ using System.Web;
 
 namespace AmericanService.Models
 {
-    public class Colaborador
+    public class Colaborador : Usuario
     {
+        public Colaborador(int cedula, string nombre, string apellidos, DateTime fecha_nacimiento) : base(cedula, nombre, apellidos, fecha_nacimiento)
+        {
+        }
+
+        public Usuario supervisor { get; set; }
+        public DateTime fecha_ingreso { get; set; }
+        public char estado { get; set; }
+        public string proyecto { get; set; }
+        public double desempeño_prueba { get; set; }
+        public double desempeño { get; set; }
+
+
     }
 }
--- AmericanService/AmericanService/Models/Usuario.cs
+++ AmericanService/AmericanService/Models/Usuario.cs
@@ -7,5 +7,17 @@ namespace AmericanService.Models
 {
     public class Usuario
     {
+        public int cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+
+        public Usuario(int cedula, string nombre, string apellidos, DateTime fecha_nacimiento)
+        {
+            this.cedula = cedula;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.fecha_nacimiento = fecha_nacimiento;
+        }
     }
 }
