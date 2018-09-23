c293a49282a5d5bd5bc72b30b2a2d28283bee98a
andressancho <36581472+andressancho@users.noreply.github.com>
2018-09-23 14:14:23 -0600

    Ejecución sp

--- AmericanService/AmericanService/Controllers/HomeController.cs
+++ AmericanService/AmericanService/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Configuration;
+using System.Data.SqlClient;
 
 namespace AmericanService.Controllers
 {
@@ -25,6 +27,37 @@ namespace AmericanService.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string userid, string pwd) //Formal Parameters  
+        {
+            string username = userid;
+            string password = pwd;
+
+            SqlConnection con = new SqlConnection(
+                WebConfigurationManager.ConnectionStrings["MyDbconn"].ConnectionString);
+
+
+            SqlCommand cmd = new SqlCommand("iniciar_sesion", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@usuario", username);
+            cmd.Parameters.AddWithValue("@contrasena", password);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            
+            con.Close();
+
+            if (username == "Admin" && password == "123456")
+            {
+                Response.Write("<h2> Success </h2> Valid User...");
+
+            }
+            else
+            {
+                Response.Write(" <h2> Failed </h2> Invalid User...");
+                return View("About");
+            }
+            return View("Index");
         }
     }
 }
