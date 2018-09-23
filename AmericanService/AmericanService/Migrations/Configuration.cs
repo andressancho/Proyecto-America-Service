d269f74c875548c03f0f9a414b1b739e2d56f962
andressancho <36581472+andressancho@users.noreply.github.com>
2018-09-22 19:59:30 -0600

    Controllers

--- AmericanService/AmericanService/Migrations/Configuration.cs
+++ AmericanService/AmericanService/Migrations/Configuration.cs
@@ -5,19 +5,13 @@ namespace AmericanService.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<AmericanService.Models.DBcontext>
+    internal sealed class Configuration 
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+           
         }
 
-        protected override void Seed(AmericanService.Models.DBcontext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
-        }
+       
     }
 }
