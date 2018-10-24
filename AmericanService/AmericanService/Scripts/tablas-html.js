c22908be81185a9370f57cdcc0da2c977eb41ece
alemolinalp <alemoli9715@gmail.com>
2018-10-23 21:53:51 -0600

    Diseño tablas

--- AmericanService/AmericanService/Scripts/tablas-html.js
+++ AmericanService/AmericanService/Scripts/tablas-html.js
@@ -1,17 +1,3 @@
 ﻿jQuery(function ($) {
-    $('.table').footable();
-
-    var prm = Sys.WebForms.PageRequestManager.getInstance();
-    if (prm != null) {
-        prm.add_endRequest(function () {
-            $(document).ready(function () {
-                $('table').footable();
-            });
-        });
-    }
-
-});
-
-jQuery(function ($) {
-    $('.table').footable();
+    $('.table').footable()
 });
